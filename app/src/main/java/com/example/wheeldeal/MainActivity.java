974f66bdb43e38795af42f3e2f7d187ba34f7741
reneekxsu <reneekxsu@gmail.com>
2021-07-26 10:01:31 -0700

    MainActivity comments and icon change

--- app/src/main/java/com/example/wheeldeal/MainActivity.java
+++ app/src/main/java/com/example/wheeldeal/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.wheeldeal;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -17,30 +17,64 @@ import com.example.wheeldeal.fragments.ProfileFragment;
 import com.example.wheeldeal.fragments.ScheduleFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+/**
+ * @brief This activity contains three fragments, accessed via the bottom navigation tab. Each
+ * fragment is represented by one of the icons on the bottom navigation tab. By default, when
+ * starting this activity, the home feed fragment will be what the user sees first. However, in the
+ * case where they have just logged in, the user will see a welcome loading screen fragment.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    // Global variable declarations
     public static final String TAG = "MainActivity";
     public BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    boolean fromLogin;
 
+    /**
+     * @brief We create and inflate the bottom navigation view. There are four fragments here—three
+     * are for the tabs, and one is for the initial loading display (where all bottom navigation
+     * tabs are shown as unchecked).
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Get a boolean flag for determining whether to display the loading fragment
         Intent intent = getIntent();
-        boolean fromLogin = intent.getExtras().getBoolean("flag");
+        fromLogin = intent.getExtras().getBoolean("flag");
 
-        final FragmentManager fragmentManager = getSupportFragmentManager();
-
-        // define your fragments here
-        final Fragment fragment1 = new HomeFragment();
-        final Fragment fragment2 = new ScheduleFragment();
-        final Fragment fragment3 = new ProfileFragment();
-        final Fragment fragment4 = new LoadingFragment();
+        // Define fragments
+        final Fragment homeFragment = new HomeFragment();
+        final Fragment scheduleFragment = new ScheduleFragment();
+        final Fragment profileFragment = new ProfileFragment();
+        final Fragment loadingFragment = new LoadingFragment();
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        // handle navigation selection
+        // Define each menu tab
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem homeTab = menu.findItem(R.id.action_home);
+        MenuItem scheduleTab = menu.findItem(R.id.action_schedule);
+        MenuItem profTab = menu.findItem(R.id.action_profile);
+
+        // Set the different home tab icons (selected and deselected)
+        Drawable homeOutline = getDrawable(R.drawable.ic_outline_home_24);
+        Drawable homeFilled = getDrawable(R.drawable.ic_baseline_home_24);
+        homeTab.setIcon(homeOutline);
+
+        // Set the different schedule tab icons (selected and deselected)
+        Drawable scheduleOutline = getDrawable(R.drawable.ic_outline_calendar_today_24);
+        Drawable scheduleFilled = getDrawable(R.drawable.ic_baseline_calendar_today_24);
+        scheduleTab.setIcon(scheduleOutline);
+
+        // Set the different profile tab icons (selected and deselected)
+        Drawable profOutline = getDrawable(R.drawable.ic_outline_person_outline_24);
+        Drawable profFilled = getDrawable(R.drawable.ic_baseline_person_24);
+        profTab.setIcon(profOutline);
+
+        // Handle navigation selection
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
@@ -48,37 +82,61 @@ public class MainActivity extends AppCompatActivity {
                         Fragment fragment;
                         switch (item.getItemId()) {
                             case R.id.action_home:
+                                // Set the home tab is checkable in case loading fragment was used
+                                // previously
                                 item.setCheckable(true);
-                                fragment = fragment1;
+                                fragment = homeFragment;
+                                // Change menu icons appearances
+                                item.setIcon(homeFilled);
+                                scheduleTab.setIcon(scheduleOutline);
+                                profTab.setIcon(profOutline);
                                 break;
                             case R.id.action_schedule:
-                                fragment = fragment2;
+                                fragment = scheduleFragment;
+                                // Change menu icons appearances
+                                item.setIcon(scheduleFilled);
+                                homeTab.setIcon(homeOutline);
+                                profTab.setIcon(profOutline);
                                 break;
                             case R.id.action_profile:
-                                fragment = fragment3;
+                                fragment = profileFragment;
+                                item.setIcon(profFilled);
+                                // Change menu icons appearances
+                                homeTab.setIcon(homeOutline);
+                                scheduleTab.setIcon(scheduleOutline);
                                 break;
                             default:
-                                fragment = fragment1;
+                                // Dummy case, since all cases are covered above. Should not reach
+                                // this case
+                                fragment = homeFragment;
                                 break;
                         }
-                        fragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
+                        // Show the selected fragment
+                        fragmentManager.beginTransaction().replace(R.id.fragmentContainer,
+                                fragment).commit();
                         return true;
                     }
                 });
-        // Set default selection
-        if (fromLogin){
-            bottomNavigationView.getMenu().getItem(0).setCheckable(false);
-            fragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment4).commit();
-        } else {
-            bottomNavigationView.setSelectedItemId(R.id.action_home);
-        }
+
+        // Start with a default fragment selected
+        setDefaultSelection(loadingFragment);
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
-        Log.i(TAG, "onActivityResult");
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 111){
-            bottomNavigationView.setSelectedItemId(R.id.action_profile);
+
+    /**
+     * @brief Sets the default selection/fragment upon launching MainActivity. We either show the
+     * loading fragment or the home fragment.
+     * @param fragment The fragment we will be showing if fromLogin is true (we expect this to be
+     *                 the loading fragment)
+     */
+    private void setDefaultSelection(Fragment fragment){
+        if (fromLogin){
+            // Deselect the default fragment tab (home fragment)
+            bottomNavigationView.getMenu().findItem(R.id.action_home).setCheckable(false);
+            // Show the loading fragment
+            fragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
+        } else {
+            // Select the home fragment tab
+            bottomNavigationView.setSelectedItemId(R.id.action_home);
         }
     }
 }
